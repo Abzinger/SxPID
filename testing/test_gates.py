7e27dd0ba93834382279580726aea134225ec597
Abzinger <abdullah.makkeh@gmail.com>
2020-03-03 18:32:45 +0100

    add new examples

--- testing/test_gates.py
+++ testing/test_gates.py
@@ -4,7 +4,7 @@ from sys import path
 path.insert(0,"../sxpid")
 
 import SxPID
-
+ 
 import time
 import pickle 
 #--------
@@ -86,6 +86,73 @@ xorcopygate[(0,1,(1,1))] = 0.25
 xorcopygate[(1,0,(0,1))] = 0.25
 xorcopygate[(1,1,(1,0))] = 0.25
 
+# RndUnqXor
+rndunqxorgate = dict()
+rndunqxorgate[(('r','a',0),('r','b',0),('r','a','b',0))] = 1/32
+rndunqxorgate[(('r','a',0),('r','b',1),('r','a','b',1))] = 1/32
+rndunqxorgate[(('r','a',1),('r','b',0),('r','a','b',1))] = 1/32
+rndunqxorgate[(('r','a',1),('r','b',1),('r','a','b',0))] = 1/32
+
+rndunqxorgate[(('r','a',0),('r','B',0),('r','a','B',0))] = 1/32
+rndunqxorgate[(('r','a',0),('r','B',1),('r','a','B',1))] = 1/32
+rndunqxorgate[(('r','a',1),('r','B',0),('r','a','B',1))] = 1/32
+rndunqxorgate[(('r','a',1),('r','B',1),('r','a','B',0))] = 1/32
+
+rndunqxorgate[(('r','A',0),('r','b',0),('r','A','b',0))] = 1/32
+rndunqxorgate[(('r','A',0),('r','b',1),('r','A','b',1))] = 1/32
+rndunqxorgate[(('r','A',1),('r','b',0),('r','A','b',1))] = 1/32
+rndunqxorgate[(('r','A',1),('r','b',1),('r','A','b',0))] = 1/32
+
+rndunqxorgate[(('r','A',0),('r','B',0),('r','A','B',0))] = 1/32
+rndunqxorgate[(('r','A',0),('r','B',1),('r','A','B',1))] = 1/32
+rndunqxorgate[(('r','A',1),('r','B',0),('r','A','B',1))] = 1/32
+rndunqxorgate[(('r','A',1),('r','B',1),('r','A','B',0))] = 1/32
+
+rndunqxorgate[(('R','a',0),('R','b',0),('R','a','b',0))] = 1/32
+rndunqxorgate[(('R','a',0),('R','b',1),('R','a','b',1))] = 1/32
+rndunqxorgate[(('R','a',1),('R','b',0),('R','a','b',1))] = 1/32
+rndunqxorgate[(('R','a',1),('R','b',1),('R','a','b',0))] = 1/32
+
+rndunqxorgate[(('R','a',0),('R','B',0),('R','a','B',0))] = 1/32
+rndunqxorgate[(('R','a',0),('R','B',1),('R','a','B',1))] = 1/32
+rndunqxorgate[(('R','a',1),('R','B',0),('R','a','B',1))] = 1/32
+rndunqxorgate[(('R','a',1),('R','B',1),('R','a','B',0))] = 1/32
+
+rndunqxorgate[(('R','A',0),('R','b',0),('R','A','b',0))] = 1/32
+rndunqxorgate[(('R','A',0),('R','b',1),('R','A','b',1))] = 1/32
+rndunqxorgate[(('R','A',1),('R','b',0),('R','A','b',1))] = 1/32
+rndunqxorgate[(('R','A',1),('R','b',1),('R','A','b',0))] = 1/32
+
+rndunqxorgate[(('R','A',0),('R','B',0),('R','A','B',0))] = 1/32
+rndunqxorgate[(('R','A',0),('R','B',1),('R','A','B',1))] = 1/32
+rndunqxorgate[(('R','A',1),('R','B',0),('R','A','B',1))] = 1/32
+rndunqxorgate[(('R','A',1),('R','B',1),('R','A','B',0))] = 1/32
+
+# Sum
+sumgate = dict()
+sumgate[(0,0,0)] = 1/4
+sumgate[(0,1,1)] = 1/4
+sumgate[(1,0,1)] = 1/4
+sumgate[(1,1,2)] = 1/4
+
+# Reduced Or
+reduorgate = dict()
+reduorgate[(0,0,0)] = 1/2
+reduorgate[(0,1,1)] = 1/4
+reduorgate[(1,0,1)] = 1/4
+
+# Rnd Xor
+rndxorgate = dict()
+rndxorgate[(('r',0),('r',0),('r',0))] = 1/8
+rndxorgate[(('r',0),('r',1),('r',1))] = 1/8
+rndxorgate[(('r',1),('r',0),('r',1))] = 1/8
+rndxorgate[(('r',1),('r',1),('r',0))] = 1/8
+
+rndxorgate[(('R',0),('R',0),('R',0))] = 1/8
+rndxorgate[(('R',0),('R',1),('R',1))] = 1/8
+rndxorgate[(('R',1),('R',0),('R',1))] = 1/8
+rndxorgate[(('R',1),('R',1),('R',0))] = 1/8
+
 gates = dict()
 gates["Xor"]  = xorgate
 gates["And"]  = andgate
@@ -96,11 +163,14 @@ gates["RndErr"]  = rnderrgate
 gates["Copy"] = copygate
 gates["(S1,Xor)"] = copyxorgate
 gates["(S2,Xor)"] = xorcopygate
-
+gates["RndUnqXor"] = rndunqxorgate
+gates["Sum"] = sumgate
+gates["ReduOr"] = reduorgate
+gates["RndXor"] = rndxorgate
 
 for gate in gates.keys():
     print("***********************************")
-    print("The JxPID for the ", gate, " :")
+    print("The SxPID for the ", gate, " :")
     print("***********************************")
     itic = time.process_time()
     SxPID.pid(n, gates[gate], lattices[n][0], lattices[n][1], True)
@@ -144,6 +214,9 @@ trihashgate[(1,0,1,0)] = 0.125
 trihashgate[(1,1,0,0)] = 0.125
 trihashgate[(1,1,1,1)] = 0.125
 
+print("***********************************")
+print("The SxPID for the three bit hash:")
+print("***********************************")
 itic = time.process_time()
 SxPID.pid(n, trihashgate, lattices[n][0], lattices[n][1], True)
 itoc = time.process_time()
@@ -172,48 +245,51 @@ quadhashgate[(1,1,0,1,1)] = 1/16
 quadhashgate[(1,1,1,0,1)] = 1/16
 quadhashgate[(1,1,1,1,0)] = 1/16
 
+print("***********************************")
+print("The SxPID for the four bit hash:")
+print("***********************************")
 itic = time.process_time()
 SxPID.pid(n, quadhashgate, lattices[n][0], lattices[n][1], True)
 itoc = time.process_time()
 print("time: ", itoc - itic, "secs")
 
-# Bivariate 4-hash
-n = 2
-biquadhashgate = dict()
-biquadhashgate[((0,0),(0,0),0)] = 1/16
-biquadhashgate[((0,0),(0,1),1)] = 1/16
-biquadhashgate[((0,0),(1,0),1)] = 1/16
-biquadhashgate[((0,0),(1,1),0)] = 1/16
-biquadhashgate[((0,1),(0,0),1)] = 1/16
-biquadhashgate[((0,1),(0,1),0)] = 1/16
-biquadhashgate[((0,1),(1,0),0)] = 1/16
-biquadhashgate[((0,1),(1,1),1)] = 1/16
-biquadhashgate[((1,0),(0,0),1)] = 1/16
-biquadhashgate[((1,0),(0,1),0)] = 1/16
-biquadhashgate[((1,0),(1,0),0)] = 1/16
-biquadhashgate[((1,0),(1,1),1)] = 1/16
-biquadhashgate[((1,1),(0,0),0)] = 1/16
-biquadhashgate[((1,1),(0,1),1)] = 1/16
-biquadhashgate[((1,1),(1,0),1)] = 1/16
-biquadhashgate[((1,1),(1,1),0)] = 1/16
+# # Bivariate 4-hash
+# n = 2
+# biquadhashgate = dict()
+# biquadhashgate[((0,0),(0,0),0)] = 1/16
+# biquadhashgate[((0,0),(0,1),1)] = 1/16
+# biquadhashgate[((0,0),(1,0),1)] = 1/16
+# biquadhashgate[((0,0),(1,1),0)] = 1/16
+# biquadhashgate[((0,1),(0,0),1)] = 1/16
+# biquadhashgate[((0,1),(0,1),0)] = 1/16
+# biquadhashgate[((0,1),(1,0),0)] = 1/16
+# biquadhashgate[((0,1),(1,1),1)] = 1/16
+# biquadhashgate[((1,0),(0,0),1)] = 1/16
+# biquadhashgate[((1,0),(0,1),0)] = 1/16
+# biquadhashgate[((1,0),(1,0),0)] = 1/16
+# biquadhashgate[((1,0),(1,1),1)] = 1/16
+# biquadhashgate[((1,1),(0,0),0)] = 1/16
+# biquadhashgate[((1,1),(0,1),1)] = 1/16
+# biquadhashgate[((1,1),(1,0),1)] = 1/16
+# biquadhashgate[((1,1),(1,1),0)] = 1/16
 
-itic = time.process_time()
-SxPID.pid(n, biquadhashgate, lattices[n][0], lattices[n][1], True)
-itoc = time.process_time()
-print("time: ", itoc - itic, "secs")
+# itic = time.process_time()
+# SxPID.pid(n, biquadhashgate, lattices[n][0], lattices[n][1], True)
+# itoc = time.process_time()
+# print("time: ", itoc - itic, "secs")
 
-# Twist bivariate hash gate 
-tbiquadhashgate = dict()
-tbiquadhashgate[((0,0),(0,0),0)] = 1/8
-tbiquadhashgate[((0,0),(0,1),1)] = 1/8
-tbiquadhashgate[((0,1),(1,0),0)] = 1/8
-tbiquadhashgate[((0,1),(1,1),1)] = 1/8
-tbiquadhashgate[((1,0),(0,0),1)] = 1/8
-tbiquadhashgate[((1,0),(0,1),0)] = 1/8
-tbiquadhashgate[((1,1),(1,0),1)] = 1/8
-tbiquadhashgate[((1,1),(1,1),0)] = 1/8
+# # Twist bivariate hash gate 
+# tbiquadhashgate = dict()
+# tbiquadhashgate[((0,0),(0,0),0)] = 1/8
+# tbiquadhashgate[((0,0),(0,1),1)] = 1/8
+# tbiquadhashgate[((0,1),(1,0),0)] = 1/8
+# tbiquadhashgate[((0,1),(1,1),1)] = 1/8
+# tbiquadhashgate[((1,0),(0,0),1)] = 1/8
+# tbiquadhashgate[((1,0),(0,1),0)] = 1/8
+# tbiquadhashgate[((1,1),(1,0),1)] = 1/8
+# tbiquadhashgate[((1,1),(1,1),0)] = 1/8
 
-itic = time.process_time()
-SxPID.pid(n, tbiquadhashgate, lattices[n][0], lattices[n][1], True)
-itoc = time.process_time()
-print("time: ", itoc - itic, "secs")
+# itic = time.process_time()
+# SxPID.pid(n, tbiquadhashgate, lattices[n][0], lattices[n][1], True)
+# itoc = time.process_time()
+# print("time: ", itoc - itic, "secs")
