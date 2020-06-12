51023329c680e7cf01afb3394b4df4e8e76940a1
Abzinger <abdullah.makkeh@gmail.com>
2020-06-12 18:14:40 +0200

    ii

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 # http://www.diveintopython3.net/packaging.html
 # https://pypi.python.org/pypi?:action=list_classifiers
 
 with open('README.txt') as file:
     long_description = file.read()
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
 
 setup(
     name='sxpid',
@@ -26,5 +29,6 @@ setup(
     "Topic :: Scientific/Engineering :: Bio-Informatics",
     "Topic :: Scientific/Engineering :: Physics",
     "Topic :: Scientific/Engineering :: Information Analysis",
-    ]
+    ],
+    install_requires=install_requires,
 )
